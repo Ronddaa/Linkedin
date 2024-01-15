d792fc8bb53e8b5b6830caad2ea3003d4148ebc4
Ronddaa <tronddaa@gmail.com>
2024-01-15 12:45:30 +0100

    add function

--- src/swiper.js
+++ src/swiper.js
@@ -26,3 +26,36 @@ new Swiper('.swiper-container', {
   }
 
 });
+
+function addSwiperClasses() {
+      const screenWidth = window.innerWidth;
+      const swiperContainer = document.getElementById('addSwiperContainer');
+      const swiperWrapper = document.getElementById('addSwiperWrapper');
+      const swiperSlide = document.getElementById('addSwiperSlide');
+
+      if (screenWidth >= 768 && screenWidth <= 1158) {
+        addClass(swiperContainer, 'swiper-container');
+        addClass(swiperWrapper, 'swiper-wrapper');
+        addClass(swiperSlide, 'swiper-slide');
+      } else {
+        removeClass(swiperContainer, 'swiper-container');
+        removeClass(swiperWrapper, 'swiper-wrapper');
+        removeClass(swiperSlide, 'swiper-slide');
+      }
+    }
+
+    function addClass(element, className) {
+      if (element && !element.classList.contains(className)) {
+        element.classList.add(className);
+      }
+    }
+
+    function removeClass(element, className) {
+      if (element && element.classList.contains(className)) {
+        element.classList.remove(className);
+      }
+    }
+
+    // Вызываем функцию при загрузке страницы и при изменении размеров окна
+    window.addEventListener('load', addSwiperClasses);
+    window.addEventListener('resize', addSwiperClasses);
